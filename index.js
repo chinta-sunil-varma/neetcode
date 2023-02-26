a9f6feb3f98b1926aaa2e9d89d23db60dec12596
Chinta Sunil Varma <160120737114.sunilvarma@gmail.com>
2023-02-26 11:00:06 +0530

    serving html file

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes/mainRoutes');
 const app = express()
 
 app.use('/',routes)
-
+app.use(express.static('frontend'))
+app.set('views',path.join(__dirname,'frontend/'))
+app.get('/html',(req,res)=>
+{
+    console.log(app.get('views'))
+    res.sendFile(app.get('views')+'index.html')
+})
 app.listen(5000,()=>
 {
     console.log('server started')
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,12 +1,7 @@
 const express = require('express');
+const app = express();
 const router=express.Router()
 
-router.get('/',(req,res)=>
-{
-    res.send('this is a get')
-})
-router.get('/:name',(req,res)=>
-{
-    res.send(`your updated name is ${req.params.name}`)
-})
+
+
 module.exports=router
